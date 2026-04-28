0da749c0b36e0245a1775d960c80b509fae237af
hennacafe <pajo.hennachristine@gmail.com>
2026-04-28 17:33:13 +0800

    "Create Models,Serializers and etc."

---
+++ records/permissions.py
@@ -0,0 +1,6 @@
+from rest_framework.permissions import BasePermission
+
+class IsAdminOrFaculty(BasePermission):
+    def has_permission(self, request, view):
+        
+        return request.user.groups.filter(name__in=['Admin', 'Faculty']).exists()
--- records/views.py
+++ records/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .models import Student
+from .serializers import StudentSerializer
+from .permissions import IsAdminOrFaculty
 
-# Create your views here.
+class StudentViewSet(ModelViewSet):
+    serializer_class = StudentSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.groups.filter(name__in=['Admin', 'Faculty']).exists():
+            return Student.objects.all()
+        return Student.objects.filter(owner=user)
+
+    def get_permissions(self):
+        
+        if self.action in ['create', 'destroy']:
+            permission_classes = [IsAdminUser]
+        elif self.action in ['update', 'partial_update']:
+            permission_classes = [IsAdminOrFaculty]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
